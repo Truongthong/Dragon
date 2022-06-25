229e562db409bd96745b2e16323e195b3400fd63
Truongthong <88825085+Truongthong@users.noreply.github.com>
2022-06-25 16:10:25 +0700

    Create Product1.cs

---
+++ Product1.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Automated_product_management_program
+{
+    class Product
+    {
+        public int Id { get; set; }
+        public string productName { get; set; }
+        public string productDesc { get; set; }
+        public int Price { get; set; }
+    }
+}
